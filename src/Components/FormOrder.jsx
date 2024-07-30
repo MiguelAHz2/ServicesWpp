17d3c59a24b227590b6b5c8b3507f091213fc8d5
Deivero-JM <sanabria020799@outlook.es>
2024-07-29 21:00:55 -0500

    EndAPi

--- src/Components/FormOrder.jsx
+++ src/Components/FormOrder.jsx
@@ -1,67 +1,116 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+//Estados
 
 // eslint-disable-next-line react/prop-types
 function FormOrder({ loaderState, setOrderStatus }) {
-    const [nombre, setnombre] = useState('')
-    const [telefono, settelefono] = useState(0)
-    const [direccion, setDirrecion] = useState('')
-    const [barrio, setBarrio] = useState('')
-    const [Pedido, setPedido] = useState('')
-    const [button, setButton] = useState(true)
-    const navigate = useNavigate();
+  const [nombre, setnombre] = useState("");
+  const [telefono, settelefono] = useState(0);
+  const [direccion, setDirrecion] = useState("");
+  const [barrio, setBarrio] = useState("");
+  const [Pedido, setPedido] = useState("");
+  const [button, setButton] = useState(true);
+  const navigate = useNavigate();
 
-    const handleSumbit = (e) => {
-        e.preventDefault();
-        if (nombre === "" || telefono <= 0 || direccion === "" || barrio === "" || Pedido === "")  {
-            return
-        }
-        loaderState(true)
+  const handleSumbit = async (e) => {
+    e.preventDefault();
 
-    setTimeout(() => {
-        const orderSucessful = true
-        setOrderStatus(orderSucessful)
-        navigate('/confirmation')
-    }, 2000)
+    //Validacion del formulario
+    if (
+      nombre === "" ||
+      telefono <= 0 ||
+      direccion === "" ||
+      barrio === "" ||
+      Pedido === ""
+    ) {
+      return;
     }
 
-    useEffect(() => {
+    loaderState(true);
 
-        if (nombre === "" || telefono <= 0 || direccion === "" || barrio === "" || Pedido === "") {
-            setButton(true)
-        } else {
-            setButton(false)
+    const horaLocal = new Date();
 
+    //Creacion del mensaje
+    const mensajeSend = `Pedido Realizado *${horaLocal.toLocaleDateString()}* *${horaLocal.toLocaleTimeString()}*:
+        %0A
+        %0ANombre: *${nombre}*
+        %0ATeléfono: ${telefono}
+        %0ADirección: *${direccion}*
+        %0ABarrio: *${barrio}*
+        %0ADetalles del pedido: %0A %09${Pedido}
+        %0A
+        %0A
+        %0A*Realizar confirmacion al cliente del pedido enviado!*
+        `;
 
-        }
+    //Parametros del api
+    const numberPhone = import.meta.env.VITE_NUMBER_PHONE;
+    const apiKey = import.meta.env.VITE_API_KEY;
+    const url = `https://api.callmebot.com/whatsapp.php?phone=+${numberPhone}&text=${mensajeSend}&apikey=${apiKey}`;
 
-    }, [nombre, telefono, direccion, barrio, Pedido])
+    try {
+      //Solucion temporal
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        mode: "no-cors",
+      });
+      console.log(response);
+    } catch (error) {
+      console.error("Error in fetch:", error);
+    }
+    
+    setTimeout(() => {
+      const orderSucessful = true;
+      setOrderStatus(orderSucessful);
+      loaderState(false);
+      navigate("/confirmation");
+    }, 2000);
+  };
 
-    return (
-        <>
-            <form className="space-y-6 rounded-lg bg-card p-6 shadow-lg"
-                onSubmit={(e) => handleSumbit(e)}
-            >
-                <div>
-                    <label
-                        className="peer-disabled:cursor-not-allowed peer-disabled:opacity-70 block text-sm font-medium text-muted   eground"
-                        htmlFor="nombre"
-                    >
-                        Nombre
-                    </label>
-                    <div className="mt-1">
-                        <input
-                            className="h-10 border text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted    eground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 block w-full rounded-md border-input bg-card px-3 py-2 text-card    eground shadow-sm focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary"
-                            type="text"
-                            id="nombre"
-                            placeholder="John Doe"
-                            required
-                            onChange={(e) => setnombre(e.target.value)}
-                        />
-                    </div>
-                </div>
-                {/* END CAMPO */}
+  //Habilitacion del formulario
+  useEffect(() => {
+    if (
+      nombre === "" ||
+      telefono <= 0 ||
+      direccion === "" ||
+      barrio === "" ||
+      Pedido === ""
+    ) {
+      setButton(true);
+    } else {
+      setButton(false);
+    }
+  }, [nombre, telefono, direccion, barrio, Pedido]);
+
+  return (
+    <>
+      <form
+        className="space-y-6 rounded-lg bg-card p-6 shadow-lg"
+        onSubmit={(e) => handleSumbit(e)}
+      >
+        <div>
+          <label
+            className="peer-disabled:cursor-not-allowed peer-disabled:opacity-70 block text-sm font-medium text-muted   eground"
+            htmlFor="nombre"
+          >
+            Nombre
+          </label>
+          <div className="mt-1">
+            <input
+              className="h-10 border text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted    eground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 block w-full rounded-md border-input bg-card px-3 py-2 text-card    eground shadow-sm focus:border-primary focus:outline-none focus:ring-1 focus:ring-primary"
+              type="text"
+              id="nombre"
+              placeholder="John Doe"
+              required
+              onChange={(e) => setnombre(e.target.value)}
+            />
+          </div>
+        </div>
+        {/* END CAMPO */}
 
         <div>
           <label
