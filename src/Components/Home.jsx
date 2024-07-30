e1941c1687f3b4ab6b7b4a6756c8f701b2ab56d6
MiguelAHz2 <mjosalvarez@poligran.edu.co>
2024-07-30 09:19:34 -0500

    Cambios

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,9 +6,9 @@ function Home() {
       <>
       <div className="flex items-center justify-center min-h-screen bg-gray-100">
     <div className="text-center">
-        <img src="/public/logo.png" alt="Logo" className="mx-auto mb-2" />
-        <h1 className="text-2xl font-bold mb-2">Emprendimiento S.A</h1>
-        <p className="text-gray-600 mb-6">Realiza tu pedido de los productos que mas te gusten!</p>
+        <img src="/logotipo.png" alt="Logo" className="mx-auto mb-2" />
+        <h1 className="text-2xl font-bold mb-2">Pedidos</h1>
+        <p className="text-gray-600 mb-6">Realiza tu pedido</p>
         <Link to="/main"><button className="bg-black text-white px-4 py-2 rounded">Toma una orden</button></Link>
     </div>
 </div>      
